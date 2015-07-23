14ed098bdb7435c32c21141ce8f8c4352114900b
Starycjusz <damianstolc@gmail.com>
2015-07-23 21:24:24 +0200

    Dodanie bootstrapa i hellobundle

---
+++ src/Lib/HelloBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Lib\HelloBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('LibHelloBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ src/Lib/HelloBundle/LibHelloBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Lib\HelloBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class LibHelloBundle extends Bundle
+{
+}
