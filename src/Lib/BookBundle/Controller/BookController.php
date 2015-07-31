86910c49f232a27323280295ae0c573a0eea39d3
Lukasz196 <exit196@gmail.com>
2015-07-31 18:25:32 +0200

    - dodawanie genee

---
+++ src/Lib/BookBundle/Controller/BookController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Lib\BookBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Lib\BookBundle\Entity\Book;
+use Lib\BookBundle\Form\Type\GenreType;
+use Lib\BookBundle\Entity\Genre;
+
+class BookController extends Controller {
+
+    public function addBookAction(Request $request) {
+
+        $book = new Book();
+
+        $form = $this->createFormBuilder($book)
+                ->add($child);
+
+        return $this->render('LibBookBundle:Librarian:addBook.html.twig');
+    }
+
+    public function addGenreAction(Request $request) {
+        $genre = new Genre();
+
+        $form = $this->createForm(new GenreType(), $genre);
+
+        $form->handleRequest($request);
+        
+        if($form->isValid()){
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($genre);
+            $em->flush();
+        }
+
+        return $this->render('LibBookBundle:Librarian:addGenre.html.twig', array(
+                    'form' => $form->createView()
+        ));
+    }
+
+}
---
+++ src/Lib/BookBundle/Form/Type/GenreType.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Lib\BookBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class GenreType extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder
+                ->add('name')
+                ->add('save', 'submit');
+    }
+
+    public function getName() {
+        return 'genre';
+    }
+
+}
